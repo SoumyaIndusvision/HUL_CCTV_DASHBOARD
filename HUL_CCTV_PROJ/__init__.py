28648528e0a5c4784733004fcc09e43ab7118413
SoumyaIndusvision <soumya@indusvision.ai>
2025-02-21 17:43:00 +0530

    modified

---
+++ HUL_CCTV_PROJ/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, unicode_literals
+
+
+from .celery import celery_app as app
+
+__all__ = ('app',)
--- HUL_CCTV_PROJ/settings.py
+++ HUL_CCTV_PROJ/settings.py
@@ -145,6 +145,7 @@ AUTH_USER_MODEL = 'users.User'
 
 # Redis for Celery
 CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 

38f3f42a2862629e3ad9ab4d817f50b427df38b4
SoumyaIndusvision <soumya@indusvision.ai>
2025-03-25 14:09:26 +0530

    modified

--- HUL_CCTV_PROJ/settings.py
+++ HUL_CCTV_PROJ/settings.py
@@ -201,6 +201,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'site', 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'site', 'static', 'build')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
